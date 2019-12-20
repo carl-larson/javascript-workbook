366dca07cc9ca5868ec236f2136676a7b27d894b
carl-larson <clarson08@gmail.com>
2019-12-19 19:29:58 -0600

    did warmup in hello.js

--- 01week/hello.js
+++ 01week/hello.js
@@ -1,9 +1,16 @@
-const name = 'Carl';
-const age = 109;
-
-let sentence = function (userName, userAge) {
-    return `${userName} is ${userAge} centuries old.`
+function longestWord(sentence) {
+    let words = sentence.split(' ');
+    let longest = words[0];
+        for(let i = 0; i < words.length - 1; i++){
+        let word2 = words[i];
+        if (longest.length < word2.length ){
+            longest = word2;
+        }
+    } 
+    console.log(`In the sentence: ${sentence}`);
+    console.log(`The longest word is: ${longest}`);
+    console.log(`This word is number ${words.indexOf(longest)+1}.`);
 }
-console.log(sentence(name, age));
 
+longestWord('Sphinx of black quartz judge my vow')
 
