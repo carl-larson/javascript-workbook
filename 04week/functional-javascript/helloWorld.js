02352d399776c5019c3a36ae772271c853527c4d
carl-larson <clarson08@gmail.com>
2020-01-14 21:16:59 -0600

    messing around in warmups, did the space travel assignment

--- 04week/functional-javascript/helloWorld.js
+++ 04week/functional-javascript/helloWorld.js
@@ -1,5 +1,33 @@
-function upperCaser(input) {
-  return input.toUpperCase();
+class address {
+  constructor (iLine1, iCity, iState, iZip) {
+    this.line1 = iLine1;
+    this.city = iCity;
+    this.state = iState;
+    this.zip = iZip;
+  }
+  generateString(){
+    return `${this.line1}
+${this.city}, ${this.state} ${this.zip}`
+  }
 }
 
-module.exports = upperCaser
+class license {
+  constructor (iName, iNumber) {
+    this.name = iName;
+    this.number = iNumber;
+  }
+  updateAddress(newAdd) {
+    this.address = newAdd;
+  }
+  makeLicense(){
+    return `${this.name}
+${this.number}
+${this.address}`;
+  }
+}
+
+let school = new address ("314 Highland Blvd", "Austin", "TX", "78752");
+let driver = new license ("Jim", "123456789");
+// console.log(school.generateString());
+driver.updateAddress(school.generateString());
+console.log(driver.makeLicense());
