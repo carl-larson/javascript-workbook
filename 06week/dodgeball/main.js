5da1063b3e3e3d0491aa7adbfa024b31cd704cd0
carl-larson <clarson08@gmail.com>
2020-02-05 19:05:57 -0600

    Think I finally finished DodgeBall correctly. Go Blue Hippos!

--- 06week/dodgeball/main.js
+++ 06week/dodgeball/main.js
@@ -57,15 +57,38 @@ const redTeam = []
 //from person list to player list:
 
 class Player {
-    constructor(canThrow, dodge, paid, healthy, experience){
+    constructor(canThrow, dodge, paid, healthy, experience, id, name, age, skill, place){
         this.canThrowBall = canThrow;
         this.canDodgeBall= dodge,
         this.hasPaid= paid,
         this.isHealthy= healthy,
         this.yearsExperience= experience
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.skillSet = skill;
+        this.placeBorn = place;
     }
-    onTeam(){
+    onTeam(team){
+        // let person = this;
+        // assignTeam(team, person);
+        if(team == 'blue'){
+            let newMember = new TeamMember(this.canThrowBall, this.canDodgeBall, this.hasPaid, this.isHealthy, this.yearsExperience, this.id, this.name, this.age, this.skillSet, this.placeBorn, 'blue', 'hippos');
+            blueTeam.push(newMember);
+        } if(team== 'red') {
+            let newMember = new TeamMember(this.canThrowBall, this.canDodgeBall, this.hasPaid, this.isHealthy, this.yearsExperience, this.id, this.name, this.age, this.skillSet, this.placeBorn, 'red', 'squirrels');
+            redTeam.push(newMember);
+        }
+        displayPlayers(team);
+        console.log("Assign Team ", team);
+    }
+}
 
+class TeamMember extends Player{
+    constructor(canThrow, dodge, paid, healthy, experience, id, name, age, skill, place, color, mascot){
+        super(canThrow, dodge, paid, healthy, experience, id, name, age, skill, place)
+        this.color = color;
+        this.mascot = mascot;
     }
 }
 // THe button from the base html file creates a list of people. The list
@@ -89,28 +112,31 @@ const listPeopleChoices = () => {
 
 // From the button next to the person, calls the makePlayer() function:
 
-function makePlayer(id) {
+function makePlayer(person) {
     // console.log(`li ${id} was clicked!`);
-    console.log(id.name);
+    console.log(person.name);
     const playerElement = document.getElementById('players');
     const redButton = document.createElement('button');
     const blueButton = document.createElement('button');
     const list = document.createElement('li');
-    const name = id.name;
+    const name = person.name;
+    
     redButton.innerHTML = 'Red Team';
     blueButton.innerHTML = 'Blue Team';
     redButton.addEventListener('click', function(){
-        assignTeam('red', id);
+        person.onTeam('red');
         playerElement.removeChild(list);
     });
     blueButton.addEventListener('click', function(){
-        assignTeam('blue', id);
+        person.onTeam('blue');
         playerElement.removeChild(list);
     });
     list.appendChild(redButton);
     list.appendChild(blueButton);
     list.appendChild(document.createTextNode(name));
     playerElement.append(list);
+    person = new Player (true, true, true, true, true, person.id, person.name, person.age, person.skill, person.place);
+    listOfPlayers.push(person);
     // const list = document.getElementById('people');
     // const li = list.getElementById(id);
     // list.removeChild(li);
@@ -118,16 +144,42 @@ function makePlayer(id) {
     // arrOfPeople.splice(id-2,1);
     
 }
-function assignTeam(team, id){
+function assignTeam(team, person){
     console.log(team);
     let blueTeam = document.getElementById('blue');
     let redTeam = document.getElementById('red');
     const list = document.createElement('li');
-    list.appendChild(document.createTextNode(id.name));
+    list.appendChild(document.createTextNode(person.name));
     if (team == 'red'){
         redTeam.append(list);
     } else {
         blueTeam.append(list);
     }
     // playerElement.
+}
+// let redMembership = 0;
+function displayPlayers(team) {
+    const showRed = document.getElementById('red');
+    const showBlue = document.getElementById('blue');
+    // let member = document.createElement('li');
+    
+    console.log(team);
+    if(team == 'red') {showRed.innerHTML="";}
+    if(team == 'blue') {showBlue.innerHTML="";}
+    if(team == 'red'){
+        
+        for (i=0; i<redTeam.length; i++){
+            console.log(i);
+            let member = document.createElement('li');
+            member.appendChild(document.createTextNode(redTeam[i].name));
+            showRed.append(member);
+        }
+    } else {
+        
+        for (i=0; i<redTeam.length; i++){
+            let member = document.createElement('li');
+            member.appendChild(document.createTextNode(blueTeam[i].name));
+            showBlue.append(member);
+        }
+    }
 }
